1fa0576003881f0f3e904d1b5939d7fb6196338c
jvancel <james.h.vancel@gmail.com>
2015-03-15 10:54:04 +0000

    SEcond Commit

---
+++ drink_maker.py
@@ -0,0 +1,9 @@
+def drink_maker(person):
+  """Asks the pirate what their taste preferences are and maps them to a drink selection"""
+  ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+  }
--- drink_menu.py
+++ drink_menu.py
@@ -7,28 +7,42 @@ questions = {
     "sweet": "Would ye like a bit of sweetness with yer poison?",
     "fruity": "Are ye one for a fruity finish?"
   }
-ingredients = {
+preferences= {}
+for flavor in questions:
+  invalid_input = False
+  value = flavor
+  while invalid_input == False:
+    flavor = raw_input(questions[flavor]+ ", answer Yes or No: ")
+    if flavor == "Yes" or flavor == "No":
+      if flavor == "No":
+        preferences[value] = False
+      else:
+        preferences[value] = True
+      invalid_input = True
+    else:
+      print "You entered an invalid response, please try again."
+
+      
+def drink_maker(preferences):
+  import random
+  """Asks the pirate what their taste preferences are and maps them to a drink selection"""
+  ingredients = {
     "strong": ["glug of rum", "slug of whisky", "splash of gin"],
     "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
     "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
     "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
   }
-flav_li_bool = []
-for flavor in questions:
-  invalid_input = False
-  while invalid_input == False:
-    flavor = raw_input(questions[flavor]+ ", answer Yes or No: ")
-    if flavor == "Yes" or flavor == "No":
-      if flavor == "No":
-        flav_li.append(False)
-      else:
-        flav_li.append(True)
-      invalid_input = True
-    else:
-      print "You entered an invalid response, please try again."
-print flav_li
+  ing_li =[]
+  for flavor in preferences:
+    if preferences[flavor] == True:
+      choice = ingredients[flavor]
+      selection = random.choice(choice)
+      ing_li.append(selection)
+  return ing_li
+if __name__ == '__main__':
+    drink_menu("James")
+    drink_maker(preferences)
 
-    
   
   
